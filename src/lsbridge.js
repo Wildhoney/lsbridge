d87143db418f2f55bdba7b3c141ec9ec2df9387f
Wildhoney <adam.timberlake@gmail.com>
2015-05-15 16:31:38 +0100

    Using onStorage event

--- src/lsbridge.js
+++ src/lsbridge.js
@@ -1,103 +1,124 @@
-(function(root, factory) {
-  if(typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if(typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    root.lsbridge = factory();
-  }
-}(this, function() {
+(function main($window, $localStorage) {
 
-  /* 
-    - Storing messages in localStorage.
-    - Clients subscribe to the changes and
-      they get notified if a new message arrives.
-  */
+    "use strict";
 
-  var api = {};
+    /**
+     * @module LSBridge
+     * @constructor
+     */
+    var LSBridge = function LSBridge() {
+        $window.addEventListener('storage', invokeListeners.bind(this));
+    };
 
-  api.isLSAvailable = (function() {
-    var mod = '_';
-    try {
-      localStorage.setItem(mod, mod);
-      localStorage.removeItem(mod);
-      return true;
-    } catch(e) {
-      return false;
-    }
-  })();
+    /**
+     * @property prototype
+     * @type {Object}
+     */
+    LSBridge.prototype = {
 
-  if(api.isLSAvailable) {
+        /**
+         * @property listeners
+         * @type {Array}
+         */
+        listeners: [],
 
-    var interval = 100
-      , intervalForRemoval = 1000
-      , ls = localStorage
-      , listeners = {}
-      , isLoopStarted = false
-      , buffer = {};
+        /**
+         * @property isLSAvailable
+         * @type {Boolean}
+         */
+        isLSAvailable: (function() {
 
-    var loop = function() {
-      for(var namespace in listeners) {
-        var data = ls.getItem(namespace);
-        if(data && buffer[namespace] && buffer[namespace].indexOf(data) === -1) {
-          buffer[namespace].push(data);
-          try {
-            var parsed = JSON.parse(data);
-            if(parsed) data = parsed;
-          } catch(e) {}
-          for(var i=0; i<listeners[namespace].length; i++) {
-            listeners[namespace][i](data);
-          }
-          if(!ls.getItem(namespace + '-removeit')) {
-            ls.setItem(namespace + '-removeit', '1');
-            (function(n) {
-              setTimeout(function() {
-                ls.removeItem(n);
-                ls.removeItem(n + '-removeit');
-                buffer[namespace] = [];
-              }, intervalForRemoval);
-            })(namespace);
-          }
-        } else if(!data) {
-          buffer[namespace] = [];
+            var mod = '_';
+
+            try {
+                $localStorage.setItem(mod, mod);
+                $localStorage.removeItem(mod);
+                return true;
+            } catch(e) {
+                return false;
+            }
+
+        })(),
+
+        /**
+         * @method send
+         * @param {String} namespace
+         * @param {*} data
+         * @return {void}
+         */
+        send: function send(namespace, data) {
+            $localStorage.setItem(namespace, serialize(data));
+        },
+
+        /**
+         * @method subscribe
+         * @param {String} namespace
+         * @param {Function} fn
+         * @return {void}
+         */
+        subscribe: function subscribe(namespace, fn) {
+            this.listeners.push({ ns: namespace, fn: fn });
         }
-      }
-      setTimeout(loop, interval);
-      return true;
+
     };
 
-    api.send = function(namespace, data) {
-      var raw = '';
-      if(typeof data === 'function') { data = data(); }
-      if(typeof data === 'object') {
-        raw = JSON.stringify(data);
-      } else {
-        raw = data;
-      }
-      ls.setItem(namespace, raw);
-    };
+    /**
+     * @method serialize
+     * @param {*} data
+     * @return {*}
+     */
+    function serialize(data) {
 
-    
-    api.subscribe = function(namespace, cb) {
-      if(!listeners[namespace]) {
-        listeners[namespace] = [];
-        buffer[namespace] = [];
-      }
-      listeners[namespace].push(cb);
-      if(!isLoopStarted) {
-        isLoopStarted = loop();
-      }
-    };
+        var serialized = '';
+        
+        switch (typeof data) {
+            case 'function': serialized = JSON.stringify(data()); break;
+            case 'object'  : serialized = JSON.stringify(data); break;
+            default        : serialized = data;
+        }
+        
+        return serialized;
 
-    api.getBuffer = function() {
-      return buffer;
     }
 
-  } else {
-    api.send = api.subscribe = function() {
-      throw new Error('localStorage not supported.');
-    }
-  }
+    /**
+     * @method unserialize
+     * @param {*} data
+     * @return {*}
+     */
+    function unserialize(data) {
 
-  return api;
-}));
+        try {
+
+            var parsed = JSON.parse(data);
+
+            if (parsed) {
+                data = parsed
+            }
+
+        } catch(e) {}
+
+        return data;
+
+    }
+
+    /**
+     * @method invokeListeners
+     * @property {StorageEvent} event
+     * @return {void}
+     */
+    function invokeListeners(event) {
+
+        this.listeners.forEach(function forEach(listener) {
+
+            if (listener.ns === event.key) {
+                listener.fn.call(this, event, unserialize(event.newValue));
+            }
+
+        });
+
+    }
+
+    $window.lsbridge = new LSBridge();
+
+})(window, window.localStorage);
